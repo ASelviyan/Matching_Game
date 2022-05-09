fa1dbfdd64d550053974d3a09687adf461330bdc
ASelviyan <angelikaselviyan5@gmail.com>
2022-05-09 15:39:43 -0600

    shuffle the array elements in the array NOT ON THE PAGE

--- js/script.js
+++ js/script.js
@@ -5,3 +5,38 @@ allCards.forEach((card) => {
     card.firstChild.classList.toggle("visibility");
   });
 });
+
+let array = [1, 1, 2, 2, 3, 4, 4, 5, 5];
+
+// for (let i = 0; i < array.length; i++) {
+//   randomNum = Math.floor(Math.random() * array.length);
+//   newArray.push(randomNum);
+// }
+
+// Fisher-Yates Shuffle Modern Algorithm
+// const shuffleArray = (array) => {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     const temp = array[i];
+//     array[i] = array[j];
+//     array[j] = temp;
+//     return array;
+//   }
+// };
+
+for (let i = array.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+array.forEach((card) => {});
+
+console.log(array);
+debugger;
+for (let i = 0; i < array.length; i++) {
+  allCards.forEach((card) => {
+    card.firstChild.innerText = array[i];
+  });
+}
