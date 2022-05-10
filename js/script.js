1a9f6948b610165a00b67c09a3db4b55099a9f73
ASelviyan <angelikaselviyan5@gmail.com>
2022-05-09 18:19:24 -0600

    store the two cards that were selected in variables

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 const allCards = document.querySelectorAll(".card");
+let com1;
+let com2;
 
 allCards.forEach((card) => {
   card.addEventListener("click", function () {
     card.firstChild.classList.toggle("visibility");
   });
+
+  card.addEventListener("click", function (e) {
+    if (!com1) {
+      com1 = e.target.firstChild.innerText;
+    } else {
+      com2 = e.target.firstChild.innerText;
+    }
+    console.log(com1);
+    console.log(com2);
+  });
 });
 
 let array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
 
-// for (let i = 0; i < array.length; i++) {
-//   randomNum = Math.floor(Math.random() * array.length);
-//   newArray.push(randomNum);
-// }
-
-// Fisher-Yates Shuffle Modern Algorithm
 // const shuffleArray = (array) => {
 //   for (let i = array.length - 1; i > 0; i--) {
 //     const j = Math.floor(Math.random() * (i + 1));
@@ -24,6 +30,8 @@ let array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
 //   }
 // };
 
+// Fisher-Yates Shuffle Modern Algorithm
+//lets the array be randomized
 for (let i = array.length - 1; i > 0; i--) {
   const j = Math.floor(Math.random() * (i + 1));
   const temp = array[i];
@@ -33,6 +41,9 @@ for (let i = array.length - 1; i > 0; i--) {
 
 console.log(array);
 
+// puts those that randomized array elements into each card box
 for (let i = 0; i < allCards.length; i++) {
   allCards[i].firstChild.innerText = array[i];
 }
+
+function winCondition() {}
