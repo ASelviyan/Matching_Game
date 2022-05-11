a7a1e51ea2b4cc013942fae54449c443c5555910
ASelviyan <angelikaselviyan5@gmail.com>
2022-05-11 14:51:49 -0600

    fixing a bug of pressing the same card to get points

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ const allCards = document.querySelectorAll(".card");
 const playerPoints = document.querySelector(".playerOnePoints");
 const winMessage = document.querySelector(".winning-message");
 const timerText = document.querySelector(".timer");
+const losingMessage = document.querySelector(".losing-message");
+const restartButton = document.querySelector(".restartButton");
+let array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
 let com1;
 let com2;
 let card1;
 let card2;
 let playerOneCounter = 0;
-let sec = 30;
+let sec = 10;
 gameState = true;
 
 // function timer() {
@@ -25,67 +28,64 @@ let timer1 = setInterval(function () {
   sec--;
   if (sec <= -1) {
     clearInterval(timer1);
+    losingMessage.classList.add("show");
   }
 }, 1000);
 
-allCards.forEach((card) => {
+allCards.forEach((card, i) => {
   card.addEventListener("click", function () {
     card.firstChild.classList.remove("visibility");
   });
-
-  card.addEventListener("click", function (e) {
-    if (!com1) {
-      com1 = e.target.firstChild.innerText;
-      card1 = e.target.firstChild;
-    } else {
-      com2 = e.target.firstChild.innerText;
-      card2 = e.target.firstChild;
-    }
-
-    console.log(com1);
-    console.log(com2);
-    if (com1 && com2) {
-      // if (e.target.firstChild.className !== "visibility") {
-      //   e.target.removeEventListener("click");
-      // }
-      if (com1 === com2) {
-        playerOneCounter++;
-        playerPoints.innerText = playerOneCounter;
-        winCondition();
-        console.log(playerOneCounter);
-        com1 = "";
-        com2 = "";
-      } else {
-        com1 = "";
-        com2 = "";
-        setTimeout(() => {
-          com1 = card1.classList.add("visibility");
-          com2 = card2.classList.add("visibility");
-        }, 200);
-      }
-    }
-  });
+  card.firstChild.id = i;
+  card.addEventListener("click", handleClick);
 });
-let array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
 
-// const shuffleArray = (array) => {
-//   for (let i = array.length - 1; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1));
-//     const temp = array[i];
-//     array[i] = array[j];
-//     array[j] = temp;
-//     return array;
-//   }
-// };
+function handleClick(e) {
+  if (!com1) {
+    com1 = e.target.firstChild.innerText;
+    card1 = e.target.firstChild;
+  } else {
+    com2 = e.target.firstChild.innerText;
+    card2 = e.target.firstChild;
+  }
+
+  if (card1.id === card2.id) {
+    return;
+  }
+  if (com1 && com2) {
+    if (com1 === com2) {
+      playerOneCounter++;
+      playerPoints.innerText = playerOneCounter;
+      card1.removeEventListener("click", handleClick);
+      card2.removeEventListener("click", handleClick);
+      winCondition();
+      console.log(playerOneCounter);
+      com1 = "";
+      com2 = "";
+    } else {
+      com1 = "";
+      com2 = "";
+      setTimeout(() => {
+        com1 = card1.classList.add("visibility");
+        com2 = card2.classList.add("visibility");
+      }, 200);
+    }
+  }
+}
 
 // Fisher-Yates Shuffle Modern Algorithm
 //lets the array be randomized
-for (let i = array.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  const temp = array[i];
-  array[i] = array[j];
-  array[j] = temp;
+
+function randomizer(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    return array;
+  }
 }
+randomizer(array);
 
 console.log(array);
 
@@ -101,4 +101,12 @@ function winCondition() {
   }
 }
 
+restartButton.addEventListener("click", resetGame);
+
+function resetGame(array) {
+  console.log("hi");
+  winMessage.classList.remove("show");
+  losingMessage.classList.remove("show");
+  randomizer(array);
+}
 // timer();
